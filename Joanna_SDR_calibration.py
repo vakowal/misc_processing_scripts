1309ba06f5e0a97e9c13865e3fa2602b706c4ffa
Ginger Kowal <gkowal@stanford.edu>
2015-10-19 18:02:58 -0400

    small bug fixes

--- Joanna_SDR_calibration.py
+++ Joanna_SDR_calibration.py
@@ -16,8 +16,11 @@ if __name__ == '__main__':
     summary_dict = pandas.read_csv(summary_table).to_dict(orient='list')
     ws_id_list = summary_dict['Watershed_ID']
 
+    before_lulc = u'C:/Users/Ginger/Documents/NatCap/GIS_local/Joanna/LULC_Salinas_CalcStats2015May13/LULC_GK/ais05nlcd06.tif'
+    after_lulc = u'C:/Users/Ginger/Documents/NatCap/GIS_local/Joanna/LULC_Salinas_CalcStats2015May13/LULC_GK/ais12nlcd11.tif'
+    
     for row in xrange(len(exp_df)):
-        run = exp_df.iloc[row].run
+        run = str(int(exp_df.iloc[row].run))
         k_param = exp_df.iloc[row].k_param
         ic_0 = exp_df.iloc[row].ic_0
         
@@ -30,8 +33,8 @@ if __name__ == '__main__':
             u'erosivity_uri': u'C:\\Users\\Ginger\\Documents\\NatCap\\GIS_local\\Joanna\\Salinas_erosivity_erodibility_coeffs\\erosivity\\eros_buff_ws.tif',
             u'ic_0_param': ic_0,
             u'k_param': k_param,
-            u'lulc_uri': u'C:/Users/Ginger/Documents/NatCap/GIS_local/Joanna/LULC_Salinas_CalcStats2015May13/LULC_GK/ais05nlcd06.tif',
-            u'results_suffix': run,
+            u'lulc_uri': after_lulc,
+            u'results_suffix': '%s_after' % run,
             u'sdr_max': u'0.8',
             u'threshold_flow_accumulation': u'3000',
             u'watersheds_uri': u'C:\\Users\\Ginger\\Documents\\NatCap\\GIS_local\\Joanna\\Salinas_erosivity_erodibility_coeffs\\SalinasHUC12prj_shp\\SalHUC12prj.shp',
@@ -41,17 +44,19 @@ if __name__ == '__main__':
     
         result_dict = {}
         output_shp = os.path.join(workspace, 'output',
-                                  'watershed_results_sdr_%d.shp' % run)
+                                  'watershed_results_sdr_%s_after.shp' % run)
         fields = ['ws_id', 'sed_export']
         with da.SearchCursor(output_shp, fields) as cursor:
-            cursor_row = cursor.next()
-            ws_id = cursor_row[0]
-            sed_export = cursor_row[1]
-            if ws_id in ws_id_list:
-                result_dict[ws_id] = sed_export
-        current_results = [result_dict[ws_id] for ws_id in\
+            for row in cursor:
+                ws_id = row[0]
+                sed_export = row[1]
+                if ws_id in ws_id_list:
+                    result_dict[ws_id] = sed_export
+        current_results = [result_dict[ws_id] for ws_id in
                            summary_dict['Watershed_ID']]
+        # post-hoc analysis: divide export by catchment area
         # write to summary table
-        summary_dict['run_%d' % run] = current_results
+        summary_dict['run_%s_11-12' % run] = current_results
         df = pandas.DataFrame(summary_dict)
-        df.to_csv(summary_table)
+        indexed = df.set_index(['Watershed_ID'])
+        indexed.to_csv(summary_table)
