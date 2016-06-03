3df3cd884d53d19e863fedfd932f547f36c51138
Ginger Kowal <ginger.kowal@gmail.com>
2016-06-03 17:39:42 -0400

    functions to translate solution to map, create marginal value rasters from livestock model outputs

--- CGIAR_raster_processing.py
+++ CGIAR_raster_processing.py
@@ -18,6 +18,72 @@ graz_areas = "C:/Users/Ginger/Dropbox/NatCap_backup/CGIAR/Peru/Spatial_data_stud
 clim_folder = "C:/Users/Ginger/Documents/NatCap/GIS_local/CGIAR/Forage_model_data/climate_and_soil/Worldclim_current"
 soil_folder = "C:/Users/Ginger/Documents/NatCap/GIS_local/CGIAR/Forage_model_data/climate_and_soil/soil_grids_processed"
 
+def translate_solution(solution_table, HRU_raster, raster_out_uri):
+    """"""
+    
+    out_datatype = 3
+    source_dataset = gdal.Open(HRU_raster)
+    band = source_dataset.GetRasterBand(1)
+    out_nodata = band.GetNoDataValue()
+    
+    sol_df = pd.read_csv(solution_table)
+    value_map = {row[3]: row[2] for row in sol_df.itertuples()}
+    pygeoprocessing.geoprocessing.reclassify_dataset_uri(
+               HRU_raster, value_map, raster_out_uri, out_datatype, out_nodata)
+    
+def create_mv_rasters(mv_table, subbasin_tif, outdir):
+    """make marginal value rasters from livestock model that can be summarized
+    by implementation unit polygons"""
+    
+    out_datatype = 6
+    source_dataset = gdal.Open(subbasin_tif)
+    band = source_dataset.GetRasterBand(1)
+    out_nodata = band.GetNoDataValue()
+    
+    mv_df = pd.read_csv(mv_table)
+    mv_grouped = mv_df.groupby(['animal', 'density'])
+    for intervention in mv_grouped.groups.keys():
+    # (each unique combination of animal and density)
+        # make lookup dictionary from mv_table
+        iv_df = mv_grouped.get_group(intervention)
+        value_map = {row[1]: row[5] for row in iv_df.itertuples()}
+        for subbasin in value_map.keys():
+            if value_map[subbasin] == 'failed':
+                value_map[subbasin] = out_nodata
+            else:
+                value_map[subbasin] = float(value_map[subbasin]) * 0.81
+        raster_out_uri = os.path.join(outdir, 'mv_%s_%s.tif' % 
+                                      (intervention[0], intervention[1]))
+        print "generating raster %s_%s" % (intervention[0], intervention[1])
+        pygeoprocessing.geoprocessing.reclassify_dataset_uri(
+            subbasin_tif, value_map, raster_out_uri, out_datatype, out_nodata)
+
+def summarize_mv_by_HRU():
+    HRU_zones = r"C:\Users\Ginger\Documents\NatCap\GIS_local\CGIAR\Peru\Other_spatial_data\HRU_BLUG-FESC-RYEG.tif"
+    zero_raster = r"C:\Users\Ginger\Documents\NatCap\GIS_local\CGIAR\Peru\Other_spatial_data\HRU_BLUG-FESC-RYEG-0.tif"
+    objectives = ['sdr', 'swy', 'swyl']
+    for obj in objectives:
+        raster_folder = "C:/Users/Ginger/Documents/NatCap/GIS_local/CGIAR/Peru/summarized_by_zone/%s_mv_rasters_6.2.16" % obj
+        arcpy.env.workspace = raster_folder
+        mv_rasters = arcpy.ListRasters()
+        save_dir = "C:/Users/Ginger/Documents/NatCap/GIS_local/CGIAR/Peru/summarized_by_zone/%s_mv_rasters_mosaic_6.2.16" % obj
+        if not os.path.exists(save_dir):
+            os.makedirs(save_dir)
+        for raster in mv_rasters:
+            inputs = [zero_raster, raster]
+            save_name = os.path.basename(raster)
+            arcpy.MosaicToNewRaster_management(inputs, save_dir, save_name, 
+                                               "", "32_BIT_FLOAT", "", 1,
+                                               "LAST", "")
+        arcpy.env.workspace = save_dir
+        mv_rasters = arcpy.ListRasters()
+        HRU_outdir = "C:/Users/Ginger/Documents/NatCap/GIS_local/CGIAR/Peru/summarized_by_zone/%s_summary_tables_by_HRU_6.1.16" % obj
+        if not os.path.exists(HRU_outdir):
+            os.makedirs(HRU_outdir)
+        summarize_by_zone(mv_rasters, HRU_zones, HRU_outdir)
+        summary_csv = "C:/Users/Ginger/Documents/NatCap/GIS_local/CGIAR/Peru/summarized_by_zone/%s_mv_by_HRU_6.1.16.csv" % obj
+        combine_tables(HRU_outdir, summary_csv)
+
 def extract_by_mask(folder, mask, outdir):
     if not os.path.exists(outdir):
         os.makedirs(outdir) 
@@ -108,7 +174,7 @@ def summarize_by_zone(raster_list, zonal_raster, outdir):
                                           + '.dbf')
         arcpy.sa.ZonalStatisticsAsTable(zonal_raster, 'VALUE', raster,
                                         intermediate_table,
-                                        statistics_type="MEAN")
+                                        statistics_type="SUM")
 
 def combine_tables(table_dir, output_csv):
     sum_dict = {}
@@ -118,7 +184,7 @@ def combine_tables(table_dir, output_csv):
         sum_dict[table[:-4]] = []
         sum_dict['zone_' + table[:-4]] = []
         fields = arcpy.ListFields(table)
-        field_names = ['Value', 'MEAN']
+        field_names = ['Value', 'SUM']
         with arcpy.da.SearchCursor(table, field_names) as cursor:
             try:
                 for row in cursor:
@@ -189,53 +255,56 @@ def process_climate_rasters(climate_dir):
     
     
 if __name__ == "__main__":
-    arcpy.env.overwriteOutput = 1
+    # arcpy.env.overwriteOutput = 1
     
-    pH_raster = "C:/Users/Ginger/Documents/NatCap/GIS_local/CGIAR/Forage_model_data/climate_and_soil/soil_grids_processed/PHIHOX_weighted_sum_0-15.tif"
-    # pH_to_h_conc(pH_raster)
-    soil_raster_folder = "C:/Users/Ginger/Documents/NatCap/GIS_local/CGIAR/Forage_model_data/climate_and_soil/soil_grids_processed"
-    soil_zones = "C:/Users/Ginger/Documents/NatCap/GIS_local/CGIAR/Forage_model_data/summarized_by_zone/bld_3_zones_grz.tif"
-    soil_outdir = "C:/Users/Ginger/Documents/NatCap/GIS_local/CGIAR/Forage_model_data/summarized_by_zone/soil_summary_tables"
-    arcpy.env.workspace = soil_raster_folder
-    soil_rasters = arcpy.ListRasters()
-    # summarize_by_zone(soil_rasters, soil_zones, soil_outdir)
-    summary_csv = "C:/Users/Ginger/Documents/NatCap/GIS_local/CGIAR/Forage_model_data/summarized_by_zone/soil_summary.csv"
-    # combine_tables(soil_outdir, summary_csv)
+    # pH_raster = "C:/Users/Ginger/Documents/NatCap/GIS_local/CGIAR/Forage_model_data/climate_and_soil/soil_grids_processed/PHIHOX_weighted_sum_0-15.tif"
+    # # pH_to_h_conc(pH_raster)
+    # soil_raster_folder = "C:/Users/Ginger/Documents/NatCap/GIS_local/CGIAR/Forage_model_data/climate_and_soil/soil_grids_processed"
+    # soil_zones = "C:/Users/Ginger/Documents/NatCap/GIS_local/CGIAR/Forage_model_data/summarized_by_zone/bld_3_zones_grz.tif"
+    # soil_outdir = "C:/Users/Ginger/Documents/NatCap/GIS_local/CGIAR/Forage_model_data/summarized_by_zone/soil_summary_tables"
+    # arcpy.env.workspace = soil_raster_folder
+    # soil_rasters = arcpy.ListRasters()
+    # # summarize_by_zone(soil_rasters, soil_zones, soil_outdir)
+    # summary_csv = "C:/Users/Ginger/Documents/NatCap/GIS_local/CGIAR/Forage_model_data/summarized_by_zone/soil_summary.csv"
+    # # combine_tables(soil_outdir, summary_csv)
     
-    climate_raster_folder = clim_folder
-    # summarize_rasters(climate_raster_folder)
-    # process_climate_rasters(climate_raster_folder)
-    climate_zones = "C:/Users/Ginger/Documents/NatCap/GIS_local/CGIAR/Forage_model_data/summarized_by_zone/mean_prec_3_zones_grz.tif"
-    clim_outdir = "C:/Users/Ginger/Documents/NatCap/GIS_local/CGIAR/Forage_model_data/summarized_by_zone/clim_summary_tables"
-    climate_dir = os.path.join(climate_raster_folder, 'converted')
-    arcpy.env.workspace = climate_dir
-    climate_rasters = arcpy.ListRasters()
-    # summarize_by_zone(climate_rasters, climate_zones, clim_outdir)
-    summary_csv = "C:/Users/Ginger/Documents/NatCap/GIS_local/CGIAR/Forage_model_data/summarized_by_zone/climate_summary.csv"
-    # combine_tables(clim_outdir, summary_csv)
+    # climate_raster_folder = clim_folder
+    # # summarize_rasters(climate_raster_folder)
+    # # process_climate_rasters(climate_raster_folder)
+    # climate_zones = "C:/Users/Ginger/Documents/NatCap/GIS_local/CGIAR/Forage_model_data/summarized_by_zone/mean_prec_3_zones_grz.tif"
+    # clim_outdir = "C:/Users/Ginger/Documents/NatCap/GIS_local/CGIAR/Forage_model_data/summarized_by_zone/clim_summary_tables"
+    # climate_dir = os.path.join(climate_raster_folder, 'converted')
+    # arcpy.env.workspace = climate_dir
+    # climate_rasters = arcpy.ListRasters()
+    # # summarize_by_zone(climate_rasters, climate_zones, clim_outdir)
+    # summary_csv = "C:/Users/Ginger/Documents/NatCap/GIS_local/CGIAR/Forage_model_data/summarized_by_zone/climate_summary.csv"
+    # # combine_tables(clim_outdir, summary_csv)
     
-    elev_raster = [r"C:\Users\Ginger\Documents\NatCap\GIS_local\CGIAR\Forage_model_data\Elevation\strm_1k_canete.tif"]
-    clim_intermediate_dir = r"C:\Users\Ginger\Documents\NatCap\GIS_local\CGIAR\Forage_model_data\summarized_by_zone\elevation_by_clim"
-    soil_intermediate_dir = r"C:\Users\Ginger\Documents\NatCap\GIS_local\CGIAR\Forage_model_data\summarized_by_zone\elevation_by_soil"
-    # summarize_by_zone(elev_raster, climate_zones, clim_intermediate_dir)
-    # summarize_by_zone(elev_raster, soil_zones, soil_intermediate_dir)
-    summary_csv = "C:/Users/Ginger/Documents/NatCap/GIS_local/CGIAR/Forage_model_data/summarized_by_zone/elev_by_clim.csv"
-    # combine_tables(clim_intermediate_dir, summary_csv)
-    summary_csv = "C:/Users/Ginger/Documents/NatCap/GIS_local/CGIAR/Forage_model_data/summarized_by_zone/elev_by_soil.csv"
-    # combine_tables(soil_intermediate_dir, summary_csv)
+    # elev_raster = [r"C:\Users\Ginger\Documents\NatCap\GIS_local\CGIAR\Forage_model_data\Elevation\strm_1k_canete.tif"]
+    # clim_intermediate_dir = r"C:\Users\Ginger\Documents\NatCap\GIS_local\CGIAR\Forage_model_data\summarized_by_zone\elevation_by_clim"
+    # soil_intermediate_dir = r"C:\Users\Ginger\Documents\NatCap\GIS_local\CGIAR\Forage_model_data\summarized_by_zone\elevation_by_soil"
+    # # summarize_by_zone(elev_raster, climate_zones, clim_intermediate_dir)
+    # # summarize_by_zone(elev_raster, soil_zones, soil_intermediate_dir)
+    # summary_csv = "C:/Users/Ginger/Documents/NatCap/GIS_local/CGIAR/Forage_model_data/summarized_by_zone/elev_by_clim.csv"
+    # # combine_tables(clim_intermediate_dir, summary_csv)
+    # summary_csv = "C:/Users/Ginger/Documents/NatCap/GIS_local/CGIAR/Forage_model_data/summarized_by_zone/elev_by_soil.csv"
+    # # combine_tables(soil_intermediate_dir, summary_csv)
     
-    pH_raster = "C:/Users/Ginger/Documents/NatCap/GIS_local/CGIAR/Forage_model_data/climate_and_soil/soil_grids_processed/PHIHOX_weighted_sum_0-15.tif"
-    pH_to_h_conc(pH_raster)
-    soil_raster_folder = "C:/Users/Ginger/Documents/NatCap/GIS_local/CGIAR/Forage_model_data/climate_and_soil/soil_grids_processed"
-    soil_outdir = "C:/Users/Ginger/Documents/NatCap/GIS_local/CGIAR/Forage_model_data/summarized_by_zone/soil_summary_tables_SWAT_subbasins"
-    arcpy.env.workspace = soil_raster_folder
-    soil_rasters = arcpy.ListRasters()
-    zone_shp = "C:/Users/Ginger/Documents/NatCap/GIS_local/CGIAR/Peru/boundaries/SWAT_subbasins.shp"
-    soil_zones = # "C:/Users/Ginger/Documents/NatCap/GIS_local/CGIAR/Forage_model_data/summarized_by_zone/bld_3_zones_grz.tif"
-    align_to = soil_rasters[0]
-    pygeoprocessing.geoprocessing.
+    # soil_raster_folder = "C:/Users/Ginger/Documents/NatCap_4.15.16/GIS_local_4.26.16/CGIAR/Peru/climate_and_soil/soil_grids_processed"
+    # soil_outdir = r"C:\Users\Ginger\Documents\NatCap_4.15.16\GIS_local_4.26.16\CGIAR\Peru\summarized_by_zone\soil_summary_tables_SWAT_subbasins"
+    # arcpy.env.workspace = soil_raster_folder
+    # soil_rasters = arcpy.ListRasters()
+    # soil_zones = r"C:\Users\Ginger\Documents\NatCap_4.15.16\GIS_local_4.26.16\CGIAR\Peru\boundaries\SWAT_subbasins.tif"
+    # # summarize_by_zone(soil_rasters, soil_zones, soil_outdir)
+    # summary_csv = "C:/Users/Ginger/Documents/NatCap_4.15.16/GIS_local_4.26.16/CGIAR/Peru/summarized_by_zone/soil_summary_SWAT_subbasins.csv"
+    # # combine_tables(soil_outdir, summary_csv)
     
-    summarize_by_zone(soil_rasters, soil_zones, soil_outdir)
-    summary_csv = "C:/Users/Ginger/Documents/NatCap/GIS_local/CGIAR/Forage_model_data/summarized_by_zone/soil_summary_SWAT_subbasins.csv"
-    combine_tables(soil_outdir, summary_csv)
+    # ## create livestock marginal value tables and summarize them by SWAT HRU
+    # mv_table = r"C:\Users\Ginger\Dropbox\NatCap_backup\CGIAR\Peru\Forage_model_results\marginal_5.2.16.csv"
+    # subbasin_tif = r"C:\Users\Ginger\Documents\NatCap\GIS_local\CGIAR\Peru\boundaries\SWAT_subbasins.tif"
+    # outdir = r"C:\Users\Ginger\Documents\NatCap\GIS_local\CGIAR\Peru\summarized_by_zone\livestock_mv_rasters_6.1.16"
+    # # create_mv_rasters(mv_table, subbasin_tif, outdir)
+    
+    summarize_mv_by_HRU()
+    
     
